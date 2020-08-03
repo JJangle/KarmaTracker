8a9d885d79145a02630b65a5c93ba3577a4bc91d
Micah Engel <micah.engel@yahoo.com>
2020-08-03 14:32:05 -0700

    added db

--- js.js
+++ js.js
@@ -1,31 +1,45 @@
-karma = 0;
-var dailyTotal = document.getElementById('dailyTotal')
+const sqlite3 = require('sqlite3').verbose();
 
-var quotes = []
+let db = new sqlite3.Database('./karma.db');
 
+let test = 'SELECT * FROM info WHERE username = ?';
 
-
-
-function goodKarma(){
-    karma++
-    dailyTotal.innerHTML = karma
-    if(karma < 0){
-        document.getElementById('circle').style.backgroundColor = 'rgba(204, 109, 109, 0.616)'
-    } else if (karma > 0){
-        document.getElementById('circle').style.backgroundColor = 'rgba(94, 151, 94, 0.459)'
-    } else {
-        document.getElementById('circle').style.backgroundColor = 'rgba(128, 128, 128, 0.199)'
+db.each(test, ['test'], (err, row) => {
+    if(err){
+        console.log(err)
     }
-}
+    console.log(row)
+});
 
-function badKarma(){
-    karma--
-    dailyTotal.innerHTML = karma
-    if(karma < 0){
-        document.getElementById('circle').style.backgroundColor = 'rgba(204, 109, 109, 0.616)'
-    } else if (karma > 0){
-        document.getElementById('circle').style.backgroundColor = 'rgba(94, 151, 94, 0.459)'
-    } else {
-        document.getElementById('circle').style.backgroundColor = 'rgba(128, 128, 128, 0.199)'
-    }
-}
+
+// karma = 0;
+// var dailyTotal = document.getElementById('dailyTotal')
+
+// var quotes = []
+
+
+
+
+// function goodKarma(){
+//     karma++
+//     dailyTotal.innerHTML = karma
+//     if(karma < 0){
+//         document.getElementById('circle').style.backgroundColor = 'rgba(204, 109, 109, 0.616)'
+//     } else if (karma > 0){
+//         document.getElementById('circle').style.backgroundColor = 'rgba(94, 151, 94, 0.459)'
+//     } else {
+//         document.getElementById('circle').style.backgroundColor = 'rgba(128, 128, 128, 0.199)'
+//     }
+// }
+
+// function badKarma(){
+//     karma--
+//     dailyTotal.innerHTML = karma
+//     if(karma < 0){
+//         document.getElementById('circle').style.backgroundColor = 'rgba(204, 109, 109, 0.616)'
+//     } else if (karma > 0){
+//         document.getElementById('circle').style.backgroundColor = 'rgba(94, 151, 94, 0.459)'
+//     } else {
+//         document.getElementById('circle').style.backgroundColor = 'rgba(128, 128, 128, 0.199)'
+//     }
+// }
